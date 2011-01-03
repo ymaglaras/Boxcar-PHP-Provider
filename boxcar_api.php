b2a072c69389b1eaae52227080d983a0d4fb57a6
Jonas Skovmand <tourettenas@tourettenas.local>
2011-01-03 17:19:12 +0100

    Fixed api call for subscribe (should be notifications/subscribe, not subscribe) and a small typo

--- boxcar_api.php
+++ boxcar_api.php
@@ -84,12 +84,12 @@ class boxcar_api {
 	 * @return bool 
 	 */
 	public function invite ($email) {
-		$result = $this->http_post('subscribe', array('email' => md5($email)));
+		$result = $this->http_post('notifications/subscribe', array('email' => md5($email)));
 		
 		if ($result['http_code'] === 404) {
 			throw new boxcar_exception('User not found', $result['http_code']);
 		} else {
-			return $this->default_response_hander($result);
+			return $this->default_response_handler($result);
 		}
 	}
 	
